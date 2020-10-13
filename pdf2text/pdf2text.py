7a324253ee8e0a749e080c76b6507262003ff146
Yahia <Yahia.Jad@gmail.com>
2020-10-13 16:17:47 +0200

    office

--- pdf2text/pdf2text.py
+++ pdf2text/pdf2text.py
@@ -3,12 +3,31 @@ import PyPDF2
 filename = r"C:\Yahia\HDB\DTS\2- Projects\VASCO\3- Execution - VASCO\Fraud- RA\Analysis sessions\OneSpan CBE Regulation Internet Banking.pdf"
 
 pdf = PyPDF2.PdfFileReader(open(filename, "rb"))
-for page in pdf.pages:
-    print (page.extractText())
+# for page in pdf.pages:
+#     print (page, page.extractText())
 
-# page1 = pdf.getPage(0)
-# print (page1)
-# for x in page1:
-#      print (x)
-#
-# print (page1.extractText())
+# print (pdf.getDocumentInfo())
+# print (pdf.getNamedDestinations())
+# print ("# of pages:", pdf.getNumPages())
+# print ("page Layout:", pdf.pageLayout(pdf.getPage(1)))
+
+page = pdf.getPage(1)
+print (page)
+#print (page.extractText())
+
+#txt = page.extractText()
+#print (type(txt))
+# for i, t in enumerate(txt):
+#     print (i, ":", t)
+
+#print (txt)
+
+# page_1 = pdf.getPage(1)
+# print (page_1)
+# for x,y in page_1.items():
+#     print (x, ":", type(x), y, ":", type(y))
+#     if type(x) == dict:
+#         for k,v in x.items():
+#             print (k, ": ", v)
+
+#print (page1.extractText())
